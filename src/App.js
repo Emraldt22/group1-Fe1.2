0f2416348abacb65a1b2e97b76e0207e03ba694b
MiggyL <116647018+MiggyL@users.noreply.github.com>
2024-07-18 19:24:05 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ function App() {
     <nav>
       <Link to="/RyojiBernardo" className="nav-item">Ryoji Bernardo</Link>
       <Link to="/JohnVincentPeregrino" className="nav-item">John Vincent Peregrino</Link>
+      <Link to="/MiguelLacanienta" className="nav-item">Miguel Lacanienta</Link>
     </nav>
     <Routes>
       <Route path="/RyojiBernardo" element={<Hero name="Ryoji Bernardo" SN="2021107273"/>} />
       <Route path="/JohnVincentPeregrino" element={<Hero name="John Vincent Peregrino" SN="2020115902"/>} />
+      <Route path="/MiguelLacanienta" element={<Hero name="Miguel Lacanienta" SN="2019130430"/>} />
     </Routes>
   </div>
   );
